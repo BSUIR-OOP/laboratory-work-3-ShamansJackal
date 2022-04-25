b354b265221df414330beea565bf1d02603a5a67
p.horovenko <p.horovenko@LINKER-W3>
2022-04-25 18:09:05 +0300

    Add enums

---
+++ lab3/Enum/EnginePowerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Enum
+{
+    public enum EnginePowerType
+    {
+        HorsePowers,
+        KgS
+    }
+}
